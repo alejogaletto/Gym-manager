5a3aaddb6eb22c33aab47c79416a1ac4945553ec
PolRivarola <rivarolapol@gmail.com>
2023-02-06 19:03:14 -0300

    students

--- gym_manager_project/apps/home/urls.py
+++ gym_manager_project/apps/home/urls.py
@@ -5,13 +5,18 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from .views import *
+
 
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+    path("home/", views.pages,name = "pages"),
+    path("students/",students,name = "students"),
+    path("staff/",staff,name = "staff"),
+    path("exercises/",exercises,name = "exercises"),
+    path("payments/",payments ,name = "payments"),
 
 ]
+
